92d6d0503407c3d04e65bdd2dc614375d4f27385
Your Name <your-github-email@example.com>
2026-09-26 00:16:34 +0530

    Update notepad app

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const notes = document.querySelector(".note-container");
+const notescontainer = document.querySelector(".note-container");
 const btn = document.querySelector(".btn");
-let input =document.querySelector(".input-box");
+let notes =document.querySelector(".input-box");
+
+btn.addEventListener("click",() =>{
+    let inputbox = document.createElement("p");
+        let img = document.createElement("img");
+        inputbox.className = "input-box";
+        inputbox.setAttribute("contenteditable", true);
+        img.src ="delete.png";
+        notescontainer.appendChild(inputbox).appendChild(img);
+        
+})
+
+
